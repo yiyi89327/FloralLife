2303c3ea248c49444a85baa7c2644348f98cb9ab
Lucifer.Han <214644001@qq.com>
2016-07-13 15:23:01 +0800

    编写了Top布局和bean类。（注意！为方便测试，启动Activity被更改）

---
+++ app/src/main/java/com/yunmeng/florallife/fragment/AuthorFragment.java
@@ -0,0 +1,32 @@
+package com.yunmeng.florallife.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yunmeng.florallife.R;
+
+/**
+ * Created by my on 2016/7/12.
+ */
+public class AuthorFragment extends Fragment {
+
+    private View view;
+
+    public static AuthorFragment newInstance() {
+        Bundle args = new Bundle();
+        AuthorFragment fragment = new AuthorFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_author, container, false);
+        return view;
+    }
+}
